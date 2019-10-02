a80b1862b7e13283da6f6f72e5a7b873cb74135b
Duong Nhat Minh <minhdemacia@gmail.com>
2019-10-02 14:19:26 +0700

    Add project files.

---
+++ WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThoat_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCong_Click(object sender, RoutedEventArgs e)
+        {
+            int a, b, c;
+            a = int.Parse(txtSoThu1.Text);
+            b = int.Parse(txtSoThu2.Text);
+            c = a + b;
+            txtKetQua.Text = c.ToString();
+            txtKetQua.Text = String.Format("{0:0.0}", c);
+        }
+
+        private void btnTru_Click(object sender, RoutedEventArgs e)
+        {
+            int a, b, c;
+            a = int.Parse(txtSoThu1.Text);
+            b = int.Parse(txtSoThu2.Text);
+            c = a - b;
+            txtKetQua.Text = c.ToString();
+            txtKetQua.Text = String.Format("{0:0.0}", c);
+        }
+
+        private void btnNhan_Click(object sender, RoutedEventArgs e)
+        {
+            int a, b, c;
+            a = int.Parse(txtSoThu1.Text);
+            b = int.Parse(txtSoThu2.Text);
+            c = a * b;
+            txtKetQua.Text = c.ToString();
+            txtKetQua.Text = String.Format("{0:0.0}", c);
+        }
+
+        private void btnChia_Click(object sender, RoutedEventArgs e)
+        {
+            float a, b, c;
+            a = float.Parse(txtSoThu1.Text);
+            b = float.Parse(txtSoThu2.Text);
+            if (b == 0)
+            {
+                MessageBox.Show("KHÔNG CHIA ĐƯỢC");
+            }
+            else
+            {
+                c = a / b;
+                //txtKetQua.Text = c.ToString();
+                txtKetQua.Text = String.Format("{0:0.0}", c);
+            }
+        }
+
+       
+    }
+}
